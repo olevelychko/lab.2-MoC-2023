6a19a7f6aee24ecde4a61bc1028f00e7c60e2b45
Olena Velychko <evkissa@gmail.com>
2023-11-30 18:52:16 +0100

    Added function for calculating affine cipher with two modes (for monograms and bigrams). Also were added support functions with alphabet of letters and bigrams.

--- src/TextCriteria.java
+++ src/TextCriteria.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class TextCriteria {
@@ -13,14 +14,44 @@ public class TextCriteria {
         return false;
     }
 
+    private static ArrayList<String> bigramAlph() {
+        String alp = "абвгдеєжзиіїйклмнопрстуфхцчшщьюя";
+        ArrayList<String> allBi = new ArrayList<>();
+        for (int i = 0; i < 32; i++) {
+            for (int j = 0; j < 32; j++) {
+                String bi = String.valueOf(alp.charAt(i)) + alp.charAt(j);
+                allBi.add(bi);
+            }
+        }
+        System.out.println(allBi);
+        return allBi;
+    }
+
+    private static int searchLet(char let) {
+        String alp = "абвгдеєжзиіїйклмнопрстуфхцчшщьюя";
+        for (int i = 0; i < alp.length(); i++) {
+            if (let == alp.charAt(i)) return i;
+        }
+        System.out.println("Can not find a char in alphabet");
+        return -1;
+    }
+
+    private static int searchBi(String bi) {
+        ArrayList<String> arrayBi = bigramAlph();
+        for (int i = 0; i < arrayBi.size(); i++) {
+            if (Objects.equals(bi, arrayBi.get(i))) return i;
+        }
+        System.out.println("Can not find a bigram in alphabet");
+        return -1;
+    }
+
 
     public static Integer numOfLetters(StringBuilder text) {
         return text.toString().length();
     }
 
-
     public static Integer numOfBigrams(StringBuilder text) {
-        int num_of_bi=0;
+        int num_of_bi = 0;
         HashMap<String, Integer> mapOfBi = new HashMap<>();
         for (int k = 0; k + 1 < text.length(); k++) {
             String bigram = text.substring(k, k + 2);
@@ -40,51 +71,75 @@ public class TextCriteria {
         return num_of_bi;
     }
 
-    public static ArrayList<String> delaytext(StringBuilder text, int l, int n)
-    {
+    public static ArrayList<String> delaytext(StringBuilder text, int l, int n) {
         ArrayList<String> delay = new ArrayList<>();
-        if(l > 100)
-        {
+        if (l > 100) {
             StringBuilder text1 = new StringBuilder();
-            for(int j = 0; j < 3; j++)
-            {
+            for (int j = 0; j < 3; j++) {
                 text1 = text.append(text);
             }
-            text = text1;
         }
-        for(int i = 0; i < n; i++)
-        {
-            delay.add(text.substring(i*l,i*l+l));
+        for (int i = 0; i < n; i++) {
+            delay.add(text.substring(i * l, i * l + l));
         }
         return delay;
     }
-    public static void Vizhiner(ArrayList<String> X, String key, int mod, String alp)
-    {
+
+    public static void Vigenere(ArrayList<String> X, String key, int mod, String alp) {
         ArrayList<String> Y = new ArrayList<>();
-        String temp = "";
+        StringBuilder temp = new StringBuilder();
         int term;
-        for(int i = 0; i < X.size(); i++)
-        {
-            for(int j = 0; j < X.get(i).length(); j++)
-            {
-                int temp1 = X.get(i).charAt(j);
-                int temp2 = key.charAt(j%mod);
-                term = ((X.get(i).charAt(j) - 1069) + (key.charAt(j%mod) - 1069))%32;
-                temp = temp + String.valueOf(alp.charAt(term));
-                term = 0;
+        for (String x : X) {
+            for (int j = 0; j < x.length(); j++) {
+                term = ((x.charAt(j) - 1069) + (key.charAt(j % mod) - 1069)) % 32;
+                temp.append(alp.charAt(term));
             }
-            Y.add(temp);
-            temp = "";
+            Y.add(temp.toString());
+            temp = new StringBuilder();
         }
+        // System.out.println(Y);
     }
 
-
-
+    public static void Affine(ArrayList<String> X, String a, String b, String alp, int deg) {
+        ArrayList<String> Y = new ArrayList<>();
+        if (deg == 1) {
+            StringBuilder temp = new StringBuilder();
+            int term;
+            int aNum = searchLet(a.charAt(0));
+            int bNum = searchLet(b.charAt(0));
+            for (String x : X) {
+                for (int j = 0; j < x.length(); j++) {
+                    int xNum = searchLet(x.charAt(j));
+                    term = (aNum * (xNum) + bNum) % 32;
+                    temp.append(alp.charAt(term));
+                }
+                Y.add(temp.toString());
+                temp = new StringBuilder();
+            }
+        } else {
+            StringBuilder temp = new StringBuilder();
+            ArrayList<String> allBi = bigramAlph();
+            int term;
+            int aNum = searchBi(a);
+            int bNum = searchBi(b);
+            for (String x : X) {
+                for (int j = 0; j + 1 < x.length(); j = j + 2) {
+                    int xNum = searchBi(x.substring(j, j + 2));
+                    term = (aNum * (xNum) + bNum) % (1024);
+                    temp.append(allBi.get(term));
+                }
+                Y.add(temp.toString());
+                temp = new StringBuilder();
+            }
+        }
+        // System.out.println(Y);
+    }
 
     public static void main(String[] args) throws Exception {
         ArrayList<Double> plainText = new ArrayList<>();
         String alp = "абвгдеєжзиіїйклмнопрстуфхцчшщьюя";
         String keyword = "метропоїзд";
+        String a = "апраршгарфдвдимдфиижюимашвфгмомищшвфдимгщиащмшишгидиідлвоимолафшярсшршршмармшардялвтмолаавм";
         File doc = new File("C:\\TextForSecondLab.txt");
         Scanner scanner = new Scanner(doc);
         StringBuilder builder = new StringBuilder();
@@ -102,7 +157,7 @@ public class TextCriteria {
         System.out.println("The number of letters is: " + numLet);
         int numBi = numOfBigrams(builder);
         System.out.println("The number of bigrams is: " + numBi);
-        ArrayList<String> tensym = delaytext(builder,10, 10000);
+        ArrayList<String> tensym = delaytext(builder, 10, 10000);
         //System.out.println(tensym);
         ArrayList<String> hundredsym = delaytext(builder, 100, 10000);
         //System.out.println(hundredsym);
@@ -110,6 +165,8 @@ public class TextCriteria {
         //System.out.println(thousandsym);
         ArrayList<String> tenthousym = delaytext(builder, 10000, 1000);
         //System.out.println(tenthousym);
-        Vizhiner(tensym, keyword, 1, alp);
+        Vigenere(tensym, keyword, 1, alp);
+        int deg = 2;
+        Affine(tensym, keyword.substring(0, deg), keyword.substring(deg, deg + deg), alp, deg);
     }
 }
